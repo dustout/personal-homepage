945c0679045ea1931e0dc9ce1a6fe3668e2fb526
Dustin <dustout@gmail.com>
2022-05-24 23:14:20 -0600

    Adding a test wasm game

--- DustinGamesterSite/Startup.cs
+++ DustinGamesterSite/Startup.cs
@@ -6,31 +6,62 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DustinGamesterSite
 {
-  public class Startup
-  {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-      services.Configure<ForwardedHeadersOptions>(options =>
-      {
-        options.ForwardedHeaders =
-            ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-      });
-    }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+              ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app)
-    {
-      app.UseForwardedHeaders();
-      app.UseDefaultFiles();
-      app.UseStaticFiles();
-      app.UseStatusCodePagesWithRedirects("/");
-    }
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+        }
 
-  }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseResponseCompression();
+
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".lua"] = "text/plain";
+            provider.Mappings[".unityweb"] = "application/octet-stream";
+            provider.Mappings[".webmanifest"] = "application/x-web-app-manifest+json";
+            provider.Mappings[".data"] = "application/octet-stream";
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ContentTypeProvider = provider,
+                OnPrepareResponse = context =>
+                {
+                    context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+                    context.Context.Response.Headers.Add("Expires", "-1");
+                }
+            });
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseStatusCodePagesWithRedirects("/");
+        }
+
+    }
 }
