77a6c2f4c43dfc8e6d0f7a5d54ca82471130140d
Dustin <dustout@gmail.com>
2023-11-26 18:25:21 -0600

    Updated winscp reference to be direct

--- DustinGamesterSite/AfterPublish.ps1
+++ DustinGamesterSite/AfterPublish.ps1
@@ -1,3 +1,3 @@
-﻿winscp.com dustingamester.com /command "call sudo systemctl stop kestrel-dustingamester.service" "synchronize remote C:\Users\dusto\source\repos\personal-homepage\DustinGamesterSite\bin\Release\netcoreapp3.1\publish /var/aspnetcore/dustingamester" "call sudo systemctl restart kestrel-dustingamester.service" "exit"
+﻿C:\Users\dusto\AppData\Local\Programs\WinSCP\winscp.com dustingamester.com /command "call sudo systemctl stop kestrel-dustingamester.service" "synchronize remote C:\Users\dusto\source\repos\personal-homepage\DustinGamesterSite\bin\Release\net8.0\publish /var/aspnetcore/dustingamester" "call sudo systemctl restart kestrel-dustingamester.service" "exit"
 start-sleep -s 5
 start https://dustingamester.com
